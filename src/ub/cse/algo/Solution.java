54ca8d5add9734eef72e29178e2a9af7d8520092
qiuhongl <qiuhongl@icloud.com>
2020-02-24 11:08:17 -0500

    Initial commit

---
+++ src/ub/cse/algo/Solution.java
@@ -0,0 +1,17 @@
+package ub.cse.algo;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Solution {
+	  private int startNode;
+	  private HashMap<Integer, ArrayList<Integer>> graph;
+	  public Solution(int node, HashMap<Integer, ArrayList<Integer>> g){
+	    startNode = node;
+	    graph = g;
+	  }
+
+	  public int[] outputDistances(){
+	    return null;
+	  }
+}
